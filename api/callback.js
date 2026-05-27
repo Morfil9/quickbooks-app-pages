1ad0a95cfd6a2bb86ea181d554b06505a525fd2d
Morfil9 <john@greenridge.io>
2026-05-27 19:10:34 +0100

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
     const params = new URLSearchParams(req.query).toString()
     res.redirect(302, `http://localhost:8765/callback?${params}`)
 }
